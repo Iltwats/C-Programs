f03798af549c809b60c0d9f7f37cc7705648d0f4
Atul Sharma <2012atulsharma@gmail.com>
2020-02-23 23:43:00 +0530

    Learn array I/O

--- Array_programs/Array.c
+++ Array_programs/Array.c
@@ -1,1 +1,24 @@
-wfw
+//Program about declaration of array(integer and character) and how to take input and output of an array.
+
+#include<stdio.h>
+int main()
+{
+    int arr[50],i,n;
+    char ch[50];
+    printf("Enter how many elements you want to take\n");
+    scanf("%d",&n);
+    printf("Enter the integer elements\n");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    getchar();
+    printf("Enter the character string\n");
+    gets(ch);
+    printf("Your entered array is:-\n");
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    printf("\nThe string you enterd is- %s \n",ch);
+}
