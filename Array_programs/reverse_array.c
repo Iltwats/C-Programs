0b5b7b08f3af4d1158da428d15c0bd039845cc14
Atul Sharma <atul.19bcon231@jecrcu.edu.in>
2020-02-23 23:22:29 +0530

    Add files via upload

---
+++ Array_programs/reverse_array.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int a[10],c,i;
+    printf("Enter the size of array\n");
+    scanf("%d",&c);
+    printf("Enter the array\n");
+    for (int i = 0; i <c; i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    printf("The array entered by you is:\t");
+    for (int i = 0; i < c; i++)
+    {
+        printf("%d ",a[i]);
+    }
+    printf("\nThe reversed array is:\t");
+   for (int i = c-1; i>=0; i--)
+   {
+       printf("%d ",a[i]);
+   }
+   
+    
+
+
+}
