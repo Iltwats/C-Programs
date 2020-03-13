cd342bf5e6a45aaf9dce39047799a075d8452a45
Atul Sharma <2012atulsharma@gmail.com>
2020-03-13 21:54:30 +0530

    update

--- Array_programs/ArraytoDigits.c
+++ Array_programs/ArraytoDigits.c
@@ -1,1 +1,22 @@
-//Programto convert 
+//Program to convert enterd array into a number
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int number=0,digit[10],numofdigits,i;
+    printf("Enter no of Digits:\n");
+    scanf("%d",&numofdigits);
+    printf("\nEnter the digits:\n");
+    for ( i = 0; i <numofdigits; i++)
+    {
+        scanf("%d",&digit[i]);
+    }
+    i=0;
+    while(i<numofdigits)
+    {
+        number=number+digit[i]*pow(10,1);
+        i++;
+    }
+    printf("\n The number is=%d",number);
+
+} 
