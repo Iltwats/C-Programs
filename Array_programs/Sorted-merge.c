6eddd077b1d7deb56ae516efc393035a9f28203e
Atul Sharma <2012atulsharma@gmail.com>
2020-03-21 00:37:09 +0530

    Create Sorted-merge.c

---
+++ Array_programs/Sorted-merge.c
@@ -0,0 +1,63 @@
+#include<stdio.h>
+int main()
+{
+    int arr1[10],arr2[10],arr3[20];
+    int i,n1,n2,m,index=0;
+    int index_first=0,index_second=0;
+    printf("Enter the no of elements in first array\n");
+    scanf("%d",&n1);
+    printf("\nEnter the elements of first array\n");
+    for ( i = 0; i < n1; i++)
+    {
+       scanf("%d",&arr1[i]);
+    }
+    printf("Enter the no of elements in second array\n");
+    scanf("%d",&n2);
+    printf("\nEnter the elements of second array\n");
+    for ( i = 0; i < n2; i++)
+    {
+       scanf("%d",&arr2[i]);
+    }
+    m=n1+n2;
+    while((index_first<n1) && (index_second<n2))
+    {
+        if(arr1[index_first]<arr2[index_second])
+        {
+            arr3[index]=arr1[index_first];
+            index_first++;
+        }
+        else
+        {
+            arr3[index]=arr2[index_second];
+            index_second++;
+        }
+        index++;
+
+    }
+    if(index_first==n1)
+    {
+        while(index_second<n2);
+        {
+            arr3[index]=arr2[index_second];
+            index_second++;
+            index++;
+        }
+    }
+    else if (index_second==n2)
+    {
+        while(index_first<n1)
+        {
+            arr3[index]=arr1[index_first];
+            index_first++;
+            index++;
+        }
+    }
+    printf("\n The merged array is ");
+    for ( i = 0; i < m; i++)
+    {
+        printf("%d ",arr3[i]);
+    }
+    
+    
+
+}
