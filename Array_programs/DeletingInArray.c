64663be4292a45002796c9529e6c62fecf40e6dd
Atul Sharma <2012atulsharma@gmail.com>
2020-03-20 21:22:59 +0530

    Create DeletingInArray.c

---
+++ Array_programs/DeletingInArray.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,pos,arr[10];
+    printf("Enter the no of elements\n");
+    scanf("%d",&n);
+    printf("Enter the elements\n");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+
+    }
+    printf("\n Enter the position from which the number has to be deleted: ");
+    scanf("%d",&pos);
+    for(i=pos;i<n-1;i++)
+    {
+        arr[i]=arr[i+1];
+
+    }
+    n--;
+    printf("\nThe array after after the deletion is:  ");
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+
+}
