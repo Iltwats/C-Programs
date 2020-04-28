68434b4775df184a998e8012ec4f276bc6bf768a
Atul Sharma <2012atulsharma@gmail.com>
2020-04-28 19:41:05 +0530

    find permutation and combination for any pair

---
+++ Functions/Permutation&Combination.c
@@ -0,0 +1,45 @@
+#include<stdio.h> 
+long permutation(int n, int r);
+long combination(int n, int r);
+long factorial(int num);
+ 
+ 
+int main(void)
+{    
+    int n, r;
+    
+    printf("Enter n: ");
+    scanf("%d", &n);
+    
+    printf("Enter r: ");
+    scanf("%d", &r);
+    
+    printf("Permutation = %ld\n", permutation(n, r));    
+    printf("Combination = %ld", combination(n, r));
+    
+    return 0; // return 0 to operating system
+}
+ 
+ 
+long permutation(int n, int r)
+{
+    return factorial(n) / factorial(n-r);
+}
+ 
+long combination(int n, int r)
+{
+    return permutation(n, r) / factorial(r);
+}
+ 
+long factorial(int num)
+{
+    long long fact = 1;
+    
+    while(num > 0)
+    {
+        fact *= num;
+        num--;
+    }
+    
+    return fact;
+}
